cf309f44022878f369337453261f0b3f40481ef7
lizka <lizka4231@gmail.com>
2025-04-14 01:20:03 +0300

    feat(*): add home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,31 @@
+import './HomePage.css';
+
+import { FC } from 'react';
+
+import CookBlog from '~/components/CookBlog';
+import Header from '~/components/Header';
+import Juiciest from '~/components/Juiciest';
+import MenuArea from '~/components/MenuArea';
+import NewRecipes from '~/components/NewRecipes';
+import Searchbar from '~/components/Searchbar';
+import StatisticsPanel from '~/components/StatisticsPanel';
+import VeganKitchen from '~/components/VeganKitchen';
+
+const HomePage: FC = () => (
+    <>
+        <Header />
+        <div className='layout'>
+            <MenuArea />
+            <main className='main'>
+                <Searchbar />
+                <NewRecipes />
+                <Juiciest />
+                <CookBlog />
+                <VeganKitchen />
+            </main>
+            <StatisticsPanel />
+        </div>
+    </>
+);
+
+export default HomePage;
